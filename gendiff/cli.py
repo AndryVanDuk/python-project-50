5151fb6f450ef62796357f51794bb5c745834313
Andrey Bezkorovayniy <duhasmok@mail.ru>
2023-04-10 20:33:12 +0300

    Fix code parser

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -1,15 +1,17 @@
 import argparse
 
 
+DESCRIPTION = 'Compares two configuration files and shows a difference.'
+
+
 def cli():
-    parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.')
-    parser.add_argument(
-        '-f',
-        '--format',
-        type=str,
-        default='stylish',
-        help='set format of output')
-    parser.add_argument('first_file', type=str)
-    parser.add_argument('second_file', type=str)
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument('first_file',
+                        help='Path to json or yml file')
+    parser.add_argument('second_file',
+                        help='Path to second file in same format')
+    parser.add_argument('-f', '--format', default='stylish',
+                        choices=['stylish', 'plain', 'json'],
+                        help='set format of output: stylish, plain or json')
     return parser.parse_args()
+
